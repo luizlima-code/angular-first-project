247ed9252b5458827ecb4c7dbe9afa0ea55c969e
Luiz Lima <luizlima.udi@gmail.com>
2021-06-15 15:01:23 -0300

    Create, Read e Update ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateUserComponent } from './users/create-user/create-user.component';
+import { UpdateComponent } from './users/update/update.component';
+import { UsersComponent } from './users/users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'users/create', component: CreateUserComponent },
+  { path: 'users/update/:id', component: UpdateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CreateUserComponent } from './users/create-user/create-user.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateComponent } from './users/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CreateUserComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
